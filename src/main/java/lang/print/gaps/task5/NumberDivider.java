c1003f10d2ab5b2bdd75dab281b111cdb445993f
Valerii Krutoshynskyi <valera9496@gmail.com>
2022-12-22 15:53:11 +0100

    done 3.5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double result = (double) toBeDivided / 5;
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds;
+        seconds = 1;
+        float halfMinutes = 30;
+        float oneMinutes = 60 * seconds;
+        float tenMinutes = 10 * oneMinutes;
+        float res = tenMinutes + oneMinutes + halfMinutes;
+        System.out.println(res);
     }
 }
